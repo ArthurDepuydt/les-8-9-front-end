40cb6b3eb731d3d8bcabbcd0145ba68363b057c2
ArthurDepuydt04 <arthur@branderij.eu>
2026-04-14 18:16:20 +0200

    klaar met de taak

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import {
   getTeVerkopen,
 } from "./helpers/storeInfo.jsx";
 
+import showOutcomeInConsole from "./constants/oefenbestand.js";
+
 import check from "./assets/check.png";
 import minus from "./assets/minus.png";
 
@@ -21,6 +23,8 @@ function App() {
   function logButton(button) {
     console.log(button);
   }
+
+  showOutcomeInConsole(inventory);
 
   return (
     <div>
@@ -51,7 +55,7 @@ function App() {
           </div>
         </section>
         <section>
-          <h2>Best verkopende televisie</h2>
+          <h2>Best verkochte televisie</h2>
           <div className="bestVerkopendeTv">
             <img
               src={bestSellingTv.sourceImg}
@@ -91,16 +95,108 @@ function App() {
             </div>
           </div>
         </section>
+
+        <section>
+          <h2>Onze merken</h2>
+          <ul>
+            {inventory.map((tv) => {
+              return <li key={tv.type}>{tv.brand}</li>;
+            })}
+          </ul>
+        </section>
         <section className="buttons">
-          <button onClick={() => logButton("Meest verkocht eerst")}>
+          <button
+            onClick={() => {
+              inventory.sort((a, b) => {
+                return b.sold - a.sold;
+              });
+              console.log(inventory);
+            }}
+          >
             Meest verkocht eerst
           </button>
-          <button onClick={() => logButton("Goedkoopste eerst")}>
+          <button
+            onClick={() => {
+              inventory.sort((a, b) => {
+                return a.price - b.price;
+              });
+              console.log(inventory);
+            }}
+          >
             Goedkoopste eerst
           </button>
-          <button onClick={() => logButton("Meest geschikt voor sport eerst")}>
+          <button
+            onClick={() => {
+              inventory.sort((a, b) => {
+                return b.refreshRate - a.refreshRate;
+              });
+              console.log(inventory);
+            }}
+          >
             Meest geschikt voor sport eerst
           </button>
+          <button
+            onClick={() => {
+              inventory.sort((x, y) => {
+                inventory.map((tv) => {
+                  let sizes = tv.availableSizes.sort((a, b) => {
+                    return b - a;
+                  });
+                  return inventory;
+                });
+                return y.availableSizes[0] - x.availableSizes[0];
+              });
+              console.log(inventory);
+            }}
+          >
+            Grootste schermgroottes eerst
+          </button>
+        </section>
+        <section>
+          <h2>Al onze televisies</h2>
+          {inventory.map((tv) => {
+            return (
+              <div className="bestVerkopendeTv">
+                <img
+                  src={tv.sourceImg}
+                  alt="Best selling TV"
+                  className="bestVerkopendeTvAfbeelding"
+                />
+                <div className="bestVerkopendeTvInfo">
+                  <div className="bestVerkopendeTitel">{getTvName(tv)}</div>
+                  <div className="bestVerkopendePrice">{getTvPrice(tv)}</div>
+                  <div>{getTvGroottes(tv)}</div>
+                  <div className="bestVerkopendeTvOpties">
+                    {tv.options.map((optie) => {
+                      if (optie.applicable == true) {
+                        return (
+                          <span key={optie.name + tv.type}>
+                            <img
+                              src={check}
+                              className="checkicon"
+                              alt="Check"
+                            />
+                            {optie.name}
+                          </span>
+                        );
+                      } else {
+                        return (
+                          <span>
+                            <img
+                              src={minus}
+                              className="checkicon"
+                              alt="Check"
+                            />
+                            {optie.name}
+                          </span>
+                        );
+                      }
+                    })}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </section>
       </main>
     </div>
